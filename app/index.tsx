c3a68e3a2cfcb712b522d0f718ebc14c458b55cb
Yohai Wiener <yohaiwiener@gmail.com>
2023-09-25 11:19:29 +0300

    set layout up

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,24 @@
-import React, { useContext } from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useContext, useRef, useState } from 'react';
+import { Button, StyleSheet, useColorScheme } from 'react-native';
 import FlatListView from '../components/DailyFlatListView';
 import HScrollView from '../components/HScrollView';
 import { Text, View } from '../components/Themed';
 import DBContext from '../context/DBLoadingContext';
 import useGetWeeklyTargets from '../hooks/useGetWeeklyTargets';
+import Colors from '../constants/Colors';
+import FilterMenu from '../components/FilterMenu';
+import { Menu } from 'react-native-popup-menu';
+import { Stack } from 'expo-router';
 
 const Home = () => {
+  const colorScheme = useColorScheme();
+
+  const [filter, setFilter] = useState<string>();
+
   const { isLoading: isDBLoading } = useContext(DBContext);
   const { weeklyTargets, isLoading, error } = useGetWeeklyTargets(isDBLoading);
+
+  const menuRef = useRef<Menu>(null);
 
   if (isLoading || isDBLoading) {
     return (
@@ -24,24 +34,48 @@ const Home = () => {
     );
   } else {
     return (
-      <View style={styles.container}>
-        <HScrollView>
-          {/* SUNDAY */}
-          <FlatListView dailyTargets={weeklyTargets![0]} />
-          {/* MONDAY */}
-          <FlatListView dailyTargets={weeklyTargets![1]} />
-          {/* TUESDAY */}
-          <FlatListView dailyTargets={weeklyTargets![2]} />
-          {/* WEDNESDAY */}
-          <FlatListView dailyTargets={weeklyTargets![3]} />
-          {/* THURSDAY */}
-          <FlatListView dailyTargets={weeklyTargets![4]} />
-          {/* FRIDAY */}
-          <FlatListView dailyTargets={weeklyTargets![5]} />
-          {/* SATURDAY */}
-          <FlatListView dailyTargets={weeklyTargets![6]} />
-        </HScrollView>
-      </View>
+      <>
+        <Stack.Screen
+          options={{
+            headerLeft: () => (
+              <Button
+                title="edit"
+                color={Colors[colorScheme ?? 'light'].accent}
+                onPress={() => ''}
+              />
+            ),
+            headerRight: () => (
+              <>
+                <Button
+                  title="filter"
+                  color={Colors[colorScheme ?? 'light'].accent}
+                  onPress={() => menuRef.current?.open()}
+                />
+
+                <FilterMenu colorScheme={colorScheme} menuRef={menuRef} setFilter={setFilter} />
+              </>
+            ),
+          }}
+        />
+        <View style={styles.container}>
+          <HScrollView>
+            {/* SUNDAY */}
+            <FlatListView dailyTargets={weeklyTargets![0]} />
+            {/* MONDAY */}
+            <FlatListView dailyTargets={weeklyTargets![1]} />
+            {/* TUESDAY */}
+            <FlatListView dailyTargets={weeklyTargets![2]} />
+            {/* WEDNESDAY */}
+            <FlatListView dailyTargets={weeklyTargets![3]} />
+            {/* THURSDAY */}
+            <FlatListView dailyTargets={weeklyTargets![4]} />
+            {/* FRIDAY */}
+            <FlatListView dailyTargets={weeklyTargets![5]} />
+            {/* SATURDAY */}
+            <FlatListView dailyTargets={weeklyTargets![6]} />
+          </HScrollView>
+        </View>
+      </>
     );
   }
 };
